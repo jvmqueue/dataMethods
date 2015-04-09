17da34f38cb992f0a5ddec270e28074ca40a3bf8
jvmqueue <wheels0090@yahoo.com>
2015-04-09 09:55:35 -0400

    define array shift and array pop button event listeners

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -5,5 +5,7 @@ require(['amdUtil'], function(util){ // want to load and use modules
 	util.fnc.causeFocus('txtValuesForArray');
 	util.fnc.setListener({$node:$('#btnPopulateArray'), event:'click', data:{'textNode':d.getElementById('txtValuesForArray')}, listener:util.fnc.populateLclArrayFromString});     
 	util.fnc.setListener({$node:$('#btnPrintRevisedArray'), event:'click', data:{'textNode':d.getElementById('txtRemoveArrayElementAt')}, listener:util.fnc.removeElementAt});
+	util.fnc.setListener({$node:$('#btnPrintAfterShift'), event:'click', data:{'textNode':null}, listener:util.fnc.arrayShift});     	
+	util.fnc.setListener({$node:$('#btnPrintAfterPop'), event:'click', data:{'textNode':null}, listener:util.fnc.arrayPop});     	
 
 });
