8faae1a45d4ee64cd355f0463da8a2b558048913
jvmqueue <wheels0090@yahoo.com>
2015-04-12 14:53:55 -0400

    let all form controls trigger the dataChange listener. The dataChange listener decides the action relative to target

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -3,9 +3,13 @@ require(['amdUtil'], function(util){ // want to load and use modules
 	var w = window, d = document, $ = jQuery;
 
 	util.fnc.causeFocus('txtValuesForArray');
-	util.fnc.setListener({$node:$('#btnPopulateArray'), event:'click', data:{'textNode':d.getElementById('txtValuesForArray')}, listener:util.fnc.populateLclArrayFromString});     
-	util.fnc.setListener({$node:$('#btnPrintRevisedArray'), event:'click', data:{'textNode':d.getElementById('txtRemoveArrayElementAt')}, listener:util.fnc.removeElementAt});
-	util.fnc.setListener({$node:$('#btnPrintAfterShift'), event:'click', data:{'textNode':null}, listener:util.fnc.arrayShift});     	
-	util.fnc.setListener({$node:$('#btnPrintAfterPop'), event:'click', data:{'textNode':null}, listener:util.fnc.arrayPop});     	
+	util.fnc.setListener({$node:$('#btnPopulateArray'), event:'click', data:{'textNode':d.getElementById('txtValuesForArray')}, listener:util.fnc.dataChange});     
+	// TODO: button removes element relative to select 
+	util.fnc.setListener({$node:$('#btnDropDwnRemoveElement'), event:'click', data:{'textNode':$('#dropDwnRemoveElement')}, listener:util.fnc.dataChange});	
+	util.fnc.setListener({$node:$('#btnPrintAfterShift'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
+	util.fnc.setListener({$node:$('#btnPrintAfterPop'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
+
+
+
 
 });
