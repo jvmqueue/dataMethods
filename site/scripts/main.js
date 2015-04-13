777ce23041b39b7f5942540619896c0c589475f2
jvmqueue <wheels0090@yahoo.com>
2015-04-13 12:27:15 -0400

    remove spaces to resolve grunt jsLint complaints

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,10 +1,8 @@
 require(['amdUtil'], function(util){ // want to load and use modules
-	
-	var w = window, d = document, $ = jQuery;
-
-	util.fnc.causeFocus('txtValuesForArray');
-	util.fnc.setListener({$node:$('#btnPopulateArray'), event:'click', data:{'textNode':d.getElementById('txtValuesForArray')}, listener:util.fnc.dataChange});     
-	util.fnc.setListener({$node:$('#btnDropDwnRemoveElement'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});	
-	util.fnc.setListener({$node:$('#btnPrintAfterShift'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
-	util.fnc.setListener({$node:$('#btnPrintAfterPop'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
+var w=window,d=document,$=jQuery;
+util.fnc.causeFocus('txtValuesForArray');
+util.fnc.setListener({$node:$('#btnPopulateArray'),event:'click',data:{'textNode':d.getElementById('txtValuesForArray')},listener:util.fnc.dataChange});
+util.fnc.setListener({$node:$('#btnDropDwnRemoveElement'),event:'click',data:{'textNode':null},listener:util.fnc.dataChange});	
+util.fnc.setListener({$node:$('#btnPrintAfterShift'),event:'click',data:{'textNode':null},listener:util.fnc.dataChange});
+util.fnc.setListener({$node:$('#btnPrintAfterPop'),event:'click',data:{'textNode':null},listener:util.fnc.dataChange});
 });
