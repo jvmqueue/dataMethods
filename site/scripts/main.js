b53a790e7d9f6c960af2bc521dc9982be4c33db0
jvmqueue <wheels0090@yahoo.com>
2015-04-13 08:48:47 -0400

    set textNode in data to null except for button populate array

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -3,13 +3,8 @@ require(['amdUtil'], function(util){ // want to load and use modules
 	var w = window, d = document, $ = jQuery;
 
 	util.fnc.causeFocus('txtValuesForArray');
-	util.fnc.setListener({$node:$('#btnPopulateArray'), event:'click', data:{'textNode':d.getElementById('txtValuesForArray')}, listener:util.fnc.dataChange});     
-	// TODO: button removes element relative to select 
-	util.fnc.setListener({$node:$('#btnDropDwnRemoveElement'), event:'click', data:{'textNode':$('#dropDwnRemoveElement')}, listener:util.fnc.dataChange});	
-	util.fnc.setListener({$node:$('#btnPrintAfterShift'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
-	util.fnc.setListener({$node:$('#btnPrintAfterPop'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
-
-
-
-
+	util.fnc.setListener({$node:$('#btnPopulateArray'), event:'click', data:{'textNode':d.getElementById('txtValuesForArray')}, listener:util.fnc.dataChange});     
+	util.fnc.setListener({$node:$('#btnDropDwnRemoveElement'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});	
+	util.fnc.setListener({$node:$('#btnPrintAfterShift'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
+	util.fnc.setListener({$node:$('#btnPrintAfterPop'), event:'click', data:{'textNode':null}, listener:util.fnc.dataChange});     	
 });
