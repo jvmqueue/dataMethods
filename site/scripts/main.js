38fbef0402b948cfc7cbb16562d4f465b5efc758
jvmqueue <wheels0090@yahoo.com>
2015-04-17 10:42:13 -0400

    define self executing preloader to decrease page background loading

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -5,4 +5,13 @@ util.fnc.setListener({$node:$('#btnPopulateArray'),event:'click',data:{'textNode
 util.fnc.setListener({$node:$('#btnDropDwnRemoveElement'),event:'click',data:{'textNode':null},listener:util.fnc.dataChange});	
 util.fnc.setListener({$node:$('#btnPrintAfterShift'),event:'click',data:{'textNode':null},listener:util.fnc.dataChange});
 util.fnc.setListener({$node:$('#btnPrintAfterPop'),event:'click',data:{'textNode':null},listener:util.fnc.dataChange});
+
+
+	var preload = (function(){
+		var arrayImages = ['main-background.jpg'];
+	    for(var i = 0, len = arrayImages.length; i < len; i++){
+	        var Img = new Image();
+	        Img.src = 'images/' + arrayImages[i];
+	    }
+	})();
 });
